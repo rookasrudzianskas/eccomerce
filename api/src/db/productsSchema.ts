707e8737b25afb69dc502db1e8fd00e435dade8e
rokas <rokas@rokas.com>
2024-10-15 17:46:44 +0200

    fix|

---
+++ api/src/db/productsSchema.ts
@@ -0,0 +1,15 @@
+import {
+    integer,
+    pgTable,
+    varchar,
+    text,
+    doublePrecision,
+  } from 'drizzle-orm/pg-core';
+  
+  export const productsTable = pgTable('products', {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    name: varchar({ length: 255 }).notNull(),
+    description: text(),
+    image: varchar({ length: 255 }),
+    price: doublePrecision().notNull(),
+  });
