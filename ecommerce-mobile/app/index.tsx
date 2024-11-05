f7326a1d6d123cb7bd929a8b53a0e97de2bd561d
rokas <rookas.rudzenskas@gmail.com>
2024-11-05 21:53:19 +0200

    fix

--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -2,15 +2,23 @@ import { View, Text, FlatList } from 'react-native';
 import products from '../assets/products.json';
 import ProductListItem from '../components/ProductListItem';
 import { Button, ButtonText } from '@/components/ui/button';
+import { useBreakpointValue } from '@/components/ui/utils/use-break-point-value';
 
 export default function HomeScreen() {
+  const numColumns = useBreakpointValue({
+    default: 2,
+    sm: 3,
+    xl: 4,
+  });
+
   return (
     <FlatList
       data={products}
-      numColumns={2}
+      key={numColumns}
+      numColumns={numColumns}
       renderItem={({ item }) => <ProductListItem product={item} />}
       keyExtractor={(item) => item.id.toString()}
-      contentContainerClassName="gap-2"
+      contentContainerClassName="gap-2 max-w-[960px] mx-auto w-full"
       columnWrapperClassName='gap-2'
     />
   );
