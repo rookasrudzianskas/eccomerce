54d90e06d4f65f90d8cd59db206db8cab45ecce4
rokas <rookas.rudzenskas@gmail.com>
2024-11-05 20:07:34 +0200

    fix

--- eccomerce-mobile/App.tsx
+++ eccomerce-mobile/App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider mode="light"><View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View></GluestackUIProvider>
   );
 }
 
