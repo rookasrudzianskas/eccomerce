554f622c61644bf387838a800a6f79f140f28d94
rokas <rookas.rudzenskas@gmail.com>
2024-11-05 19:48:46 +0200

    fix

--- eccomerce-mobile/app/index.tsx
+++ eccomerce-mobile/app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function HomeScreen() {
   return (
     <View>
-      <Text style={{ fontSize: 30 }}>Home screen</Text>
+      <Text style={{ fontSize: 30, display: "flex" }}>Home screen</Text>
     </View>
   );
 }
