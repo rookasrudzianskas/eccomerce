65a53be64bf10f8ae9a4dbc6b7758f4071b1638c
rokas <rookas.rudzenskas@gmail.com>
2024-11-05 20:45:42 +0200

    fix

--- ecommerce-mobile/components/ProductListItem.tsx
+++ ecommerce-mobile/components/ProductListItem.tsx
@@ -5,6 +5,8 @@ import { Heading } from './ui/heading';
 import { Text } from './ui/text';
 import { Box } from './ui/box';
 import { Button, ButtonText } from './ui/button';
+import { Link, router, useRouter } from 'expo-router';
+import { TouchableOpacity } from 'react-native';
 
 
 interface Product {
@@ -16,38 +18,25 @@ interface Product {
 }
 
 export default function ProductListItem({ product }: { product: Product }) {
+  const router = useRouter()
   return (
-    <Card className="p-5 rounded-lg max-w-[360px] flex-1">
-      <Image
-        source={{
-          uri: product.image,
-        }}
-        className="mb-6 h-[240px] w-full rounded-md"
-        alt={`${product.name} image`}
-        resizeMode="contain"
-      />
-      <Text className="text-sm font-normal mb-2 text-typography-700">
-        {product.name}
-      </Text>
-      <VStack className="mb-6">
-        <Heading size="md" className="mb-4">
-          ${product.price}
-        </Heading>
-        <Text size="sm">{product.description}</Text>
-      </VStack>
-      <Box className="flex-col sm:flex-row">
-        <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
-          <ButtonText size="sm">Add to cart</ButtonText>
-        </Button>
-        <Button
-          variant="outline"
-          className="px-4 py-2 border-outline-300 sm:flex-1"
-        >
-          <ButtonText size="sm" className="text-typography-600">
-            Wishlist
-          </ButtonText>
-        </Button>
-      </Box>
-    </Card>
+      <TouchableOpacity activeOpacity={0.7} onPress={() => router.push(`/product/${product.id}`)} className="flex-1">
+        <Card className="p-5 rounded-lg max-w-[360px] flex-1">
+          <Image
+            source={{
+              uri: product.image,
+            }}
+            className="mb-6 h-[240px] w-full rounded-md"
+            alt={`${product.name} image`}
+            resizeMode="contain"
+          />
+          <Text className="text-sm font-normal mb-2 text-typography-700">
+            {product.name}
+          </Text>
+          <Heading size="md" className="mb-4">
+            ${product.price}
+          </Heading>
+        </Card>
+      </TouchableOpacity>
   );
 }
