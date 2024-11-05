85bf0419fca3a046a79ba3a805497ff5b5f93f8e
rokas <rookas.rudzenskas@gmail.com>
2024-11-05 22:25:49 +0200

    fix

---
+++ ecommerce-mobile/api/products.ts
@@ -0,0 +1,21 @@
+const API_URL = process.env.EXPO_PUBLIC_API_URL;
+
+export async function listProducts() {
+    const res = await fetch(`${API_URL}/products`);
+    const data = res.json();
+
+    if(!res.ok) {
+        throw new Error('Error');
+    }
+    return data;
+}
+
+export async function fetchProductById(id: number) {
+    const res = await fetch(`${API_URL}/products/${id}`);
+    const data = res.json();
+
+    if(!res.ok) {
+        throw new Error('Error');
+    }
+    return data;
+}
