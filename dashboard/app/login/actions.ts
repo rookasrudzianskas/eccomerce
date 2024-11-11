1c1113da65dc1ed0a929220fadbc1df15fbab665
rokas <rookas.rudzenskas@gmail.com>
2024-11-11 15:19:25 +0100

    fix

--- dashboard/app/login/actions.ts
+++ dashboard/app/login/actions.ts
@@ -1,9 +1,36 @@
 'use server';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
+import {login, signup} from "@/api/auth";
 export async function handleLogin(email: string, password: string) {
-
+  let redirectUrl = `/login?errorMessage=${encodeURIComponent(
+    'Failed to login'
+  )}`;
+  try {
+    const res = await login(email, password);
+    if (res.token) {
+      cookies().set('token', res.token);
+      redirectUrl = '/dashboard';
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    redirect(redirectUrl);
+  }
 }
 export async function handleSignup(email: string, password: string) {
-
+  let redirectUrl = `/login?errorMessage=${encodeURIComponent(
+    'Failed to signup'
+  )}`;
+  try {
+    const res = await signup(email, password);
+    if (res.token) {
+      cookies().set('token', res.token);
+      redirectUrl = '/dashboard';
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    redirect(redirectUrl);
+  }
 }
