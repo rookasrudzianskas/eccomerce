6a4722e64124c197314396ea1d3ee2f6efbc2a3c
rokas <rookas.rudzenskas@gmail.com>
2024-11-11 19:51:44 +0100

    fix

---
+++ dashboard/api/orders.ts
@@ -0,0 +1,44 @@
+import { API_URL } from '@/config';
+import { cookies } from 'next/headers';
+
+export async function fetchOrders() {
+  try {
+    const token = cookies().get('token')?.value;
+    const response = await fetch(`${API_URL}/orders`, {
+      headers: {
+        Authorization: token ?? '',
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      console.error(response);
+      throw new Error('Failed to fetch orders');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function fetchOrder(id: number) {
+  try {
+    const token = cookies().get('token')?.value;
+    const response = await fetch(`${API_URL}/orders/${id}`, {
+      headers: {
+        Authorization: token ?? '',
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      console.error(response);
+      throw new Error('Failed to fetch orders');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
--- dashboard/app/dashboard/products/page.tsx
+++ dashboard/app/dashboard/products/page.tsx
@@ -8,8 +8,12 @@ export default async function ProductsPage() {
   const products = await listProducts();
   return (
     <div className="flex flex-row flex-wrap gap-4 max-w-[1400px] mx-auto w-full">
-      <Link href={'/dashboard/products/create'}>
-        <Card className={'w-full h-full min-w-[300px] items-center justify-center flex'}>
+      <div className="flex flex-row flex-wrap gap-4 max-w-[1400px] w-full">
+        <Link
+          href="/dashboard/products/create"
+          className="flex flex-1 min-w-[300px]"
+        >
+          <Card className="w-full h-full p-5 flex items-center justify-center">
           <Icon as={AddIcon} className={'w-10 h-10 text-sky-500'} />
         </Card>
       </Link>
