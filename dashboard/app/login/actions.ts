47803390f24e50ebc7eafa321b63fded87fe671d
rokas <rookas.rudzenskas@gmail.com>
2024-11-11 15:02:01 +0100

    fix

---
+++ dashboard/app/login/actions.ts
@@ -0,0 +1,9 @@
+'use server';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+export async function handleLogin(email: string, password: string) {
+
+}
+export async function handleSignup(email: string, password: string) {
+
+}
