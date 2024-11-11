ae7ee0a6d1cbcdd60d034c1484b3d35e6cbd4e1b
rokas <rookas.rudzenskas@gmail.com>
2024-11-11 17:03:33 +0100

    Fix

--- dashboard/app/dashboard/products/page.tsx
+++ dashboard/app/dashboard/products/page.tsx
@@ -1,10 +1,17 @@
 import { listProducts } from '@/api/products';
 import ProductListItem from './ProductListItem';
+import Link from "next/link";
+import {Card} from "@/components/ui/card";
 
 export default async function ProductsPage() {
   const products = await listProducts();
   return (
     <div className="flex flex-row flex-wrap gap-4 max-w-[1400px] mx-auto w-full">
+      <Link href={'/dashboard/products/create'}>
+        <Card className={'w-full h-full min-w-[300px]'}>
+
+        </Card>
+      </Link>
       {products.map((product) => (
         <ProductListItem key={product.id} product={product} />
       ))}
